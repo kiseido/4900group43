2c1fe7964d24c14dbbe72fdb38025847ca7f6f42
kiseido <kiseido@users.noreply.github.com>
2019-04-25 15:55:40 -0700

    swapped to c

--- 4900group43.c
+++ 4900group43.c
@@ -1,11 +1,14 @@
 // 4900group43.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	printf_s("Hello");
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
