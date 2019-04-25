524c47e79457852256b3158e09990ad5e2763d77
kiseido <kiseido@users.noreply.github.com>
2019-04-25 16:50:04 -0700

    working triangle

--- 4900group43.c
+++ 4900group43.c
@@ -2,13 +2,57 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <GL/glut.h>
 
-#include <GL/glew.h>
+void myInit();
+void myDraw();
 
-int main()
+main(int argc, char *argv[])
 {
-	printf_s("Hello");
+
+	/* Initialize window system */
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutCreateWindow("Hello World");
+
+	/* Initialize graphics */
+	myInit();
+
+	/* Display callback and enter event loop */
+	glutDisplayFunc(&myDraw);
+	glutMainLoop();
+}
+
+void myInit()
+{
+	/* Background color */
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	/* Projection */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
+}
+
+void myDraw()
+{
+	/* Clear the screen */
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Example code to draw 3 white points /
+	/* Draw your points here **/
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(0.25, 0.25, 0.0);
+	glVertex3f(0.75, 0.25, 0.0);
+	glVertex3f(0.75, 0.75, 0.0);
+
+	glEnd();
+
+	/* Execute draw commands */
+	glFlush();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
